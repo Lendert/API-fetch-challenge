3afa6ffb54321a1b40c9342c9208ae05d5563651
Lendert <lendertpauwels~gmail.com>
2019-06-06 14:39:33 +0200

    added description and list

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-fetch('https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6', {mode: 'cors'}) 
+const container =  document.getElementById("container");
+const info = document.getElementById("info");
+
+for (var i = 1; i <= 13; i++) {
+
+fetch(`https://api.punkapi.com/v2/beers?page=${i}`, {mode: 'cors'}) 
 .then(function (data){
    return data.json();
 })
 .then(function(beers){
-   
-    const container =  document.getElementById("container");
-    const info = document.getElementById("info");
+    
+    
     for( let beer of beers) { 
         let text = `<div class="card m-2" style="width: 18rem;">
-        <div class="text-center mt-2" style="width: 100%;">
-        <img class="mx-auto" src="${beer.image_url}" alt="Card image cap" height= "150px"  >
+        <div class="text-center bg-light" style="width: 100%;">
+        <img class="mx-auto my-3" src="${beer.image_url}" alt="${beer.name}" height= "150px"  >
         </div>
         <div class="card-body text-center">
           <h5 class="card-title">${beer.name}</h5>
           <h6 class="card-subtitle mb-2 text-muted">${beer.tagline}</h6>
+          <h6 class="card-
           <a href="#" class="btn btn-dark">more info</a>
         </div>
       </div>`;
@@ -22,17 +27,26 @@ fetch('https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6', {mode: 
           button.addEventListener("click", function(){
       container.classList.toggle("d-none");
       container.classList.toggle("d-flex");
-      info.classList.toggle("d-none")
+      info.classList.toggle("d-none");
+      info.classList.toggle("d-flex");
        
-      info.innerHTML = `<div class="card m-2">
-      <div class="row no-gutters">
-      <div class="text-center pt-2 col-md-4">
-      <img class="mx-auto" src="${beer.image_url}" alt="Card image cap" height= "150px"  >
+      info.innerHTML = `<div class="card m-4" style= "width: 600px;">
+      <div class="row no-gutters py-4">
+      <div class="text-center col-md-4">
+      <img class="mx-auto bg-dark" src="${beer.image_url}" alt="${beer.name}" height= "150px"  >
       </div>
       <div class="col-md-8">
       <div class="card-body text-center">
         <h5 class="card-title">${beer.name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${beer.tagline}</h6>
+        <h6 class="card-subtitle mb-2">${beer.description}</h6>
+        <ul class="list-group">
+        <li class="list-group-item"> ${beer.abv}</li>
+        <li class="list-group-item"></li>
+        <li class="list-group-item"></li>
+        <li class="list-group-item"></li>
+        </li>
+        </ul>
         <a href="#" class="btn btn-dark">more info</a>
         </div>
       </div>
@@ -43,7 +57,7 @@ fetch('https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6', {mode: 
       container.appendChild(child);
     }
 })
-
+}
 function parseHTML(temp){
 
  let tempdoc = document.implementation.createHTMLDocument(); 
